d153b7943921b94bc15139d1eb653f16f3afd2a5
Ivan Avdeev <me@w23.ru>
2020-03-06 01:35:59 -0800

    add plane text experiment

--- raymarch.glsl
+++ raymarch.glsl
@@ -43,17 +43,91 @@ void xsph(vec3 sc, float sr2, int mi, float ss) {
 	}
 }
 
-vec3 hsv2rgb(vec3 c)
-{
+vec3 hsv2rgb(vec3 c) {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
     return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+const float C_A = 434073., C_B = 497559., C_C = 397590., C_D = 498071.,C_E = 988959., C_F = 988945., C_G = 400790., C_H = 630681.,C_I = 467495., C_J = 467491., C_K = 611161., C_L = 69919.,C_M = 653721., C_N = 638361., C_O = 432534., C_P = 497425.,C_Q = 432606., C_R = 497497., C_S = 923271., C_T = 991778.,C_U = 629142., C_V = 629075., C_W = 646615., C_X = 628377.,C_Y = 628292., C_Z = 1016879., C_1 = 291919., C_2 = 493087.,C_3 = 495239., C_4 = 630408., C_5 = 988807., C_6 = 272278.,C_7 = 1016900., C_8 = 431766., C_9 = 433730., C_0 = 433590.,C_dot = 1024.;
+
+float gB(float g, vec2 gp){
+	return (gp.x<4.&&gp.y<5.) ? mod(floor(g / pow(2., gp.y*4. + gp.x)), 2.) : 0.;
+}
+
+#define PC(g) if(pc.x==lx){col=gB(g,pg);}lx+=1.
+
+float diGlyph(in float di) {
+	if (di == 0.) return C_0;
+	if (di == 1.) return C_1;
+	if (di == 2.) return C_2;
+	if (di == 3.) return C_3;
+	if (di == 4.) return C_4;
+	if (di == 5.) return C_5;
+	if (di == 6.) return C_6;
+	if (di == 7.) return C_7;
+	if (di == 8.) return C_8;
+	if (di == 9.) return C_9;
+	return C_E;
+}
+void printInt(in float num, in vec2 pg, in vec2 pc, inout float lx, inout float col) {
+	/*if (num < 0.) {PC(C_N);
+	num *= -1.;
+	} else {PC(diGlyph(mod(floor(num/1000.),10.)));
+}*/
+
+	if (num >= 1000.) { PC(diGlyph(mod(floor(num/1000.),10.))); }
+	if (num >= 100.) { PC(diGlyph(mod(floor(num/100.),10.))); }
+	if (num >= 10.) { PC(diGlyph(mod(floor(num/10.),10.))); }
+	PC(diGlyph(mod(floor(num),10.)));
+}
+
+float printText(vec2 p) {
+#define PIXSZ 2.
+	p = floor(p / PIXSZ);
+	vec2 pc = floor(p / vec2(5.,6.));
+	vec2 pg = mod(p, vec2(5.,6.));
+	float lx = 1.;
+	float col = 0.;
+
+#define PUTN(n) printInt(n,pg,pc,lx,col)
+	//float rnd = floor(hash1(floor(t/8.)) * 10.);
+	float rnd = mod(floor(t/8.), 11.);
+	if (pc.y == 0.) {
+	PUTN(t);
+	PC(0.);
+	PC(0.);
+	PC(0.);
+	//PC(C_L); PC(C_E); PC(C_V); PC(C_E); PC(C_L); PC(0.);
+	if (rnd == 0.) { PC(C_L);PC(C_O);PC(C_G);PC(C_I);PC(C_C);PC(C_O);PC(C_M);PC(C_A); }
+	else if (rnd == 1.) { PC(C_F);PC(C_A);PC(C_R);PC(C_B);PC(C_R);PC(C_A);PC(C_U);PC(C_S);PC(C_C);PC(C_H); }
+	else if (rnd == 2.) { PC(C_L);PC(C_J); }
+	else if (rnd == 3.) { PC(C_P);PC(C_R);PC(C_I);PC(C_S);PC(C_M);PC(C_B);PC(C_E);PC(C_I);PC(C_N);PC(C_G);PC(C_S); }
+	else if (rnd == 4.) { PC(C_A);PC(C_L);PC(C_C);PC(C_A);PC(C_T);PC(C_R);PC(C_A);PC(C_Z); }
+	else if (rnd == 5.) { PC(C_C);PC(C_O);PC(C_N);PC(C_S);PC(C_P);PC(C_I);PC(C_R);PC(C_A);PC(C_C);PC(C_Y); }
+	else if (rnd == 6.) { PC(C_Q);PC(C_U);PC(C_I);PC(C_T);PC(C_E); }
+	else if (rnd == 7.) { PC(C_M);PC(C_E);PC(C_R);PC(C_C);PC(C_R);PC(C_U);PC(C_R);PC(C_Y); }
+	else if (rnd == 8.) { PC(C_S);PC(C_A);PC(C_N);PC(C_D);PC(C_S); }
+	else if (rnd == 9.) { PC(C_T);PC(C_I);PC(C_T);PC(C_A);PC(C_N); }
+	else if (rnd == 10.) { PC(C_T);PC(C_H);PC(C_R);PC(C_O);PC(C_B); }
+	} else if (pc.y == 1.) {
+	}
+
+	return col;
+}
+
+bool mask(vec2 p) {
+	p.x += 32.;
+	float g = printText(p);
+	g *= step(hash2(floor((p+floor(vec2(.4,.7)*t))/PIXSZ)), .8);
+	return g > 0.;
 }
 
 void main() {
 	vec2 uv=(gl_FragCoord.xy/R)*2.-1.;uv.x*=R.x/R.y;
 
 	//gl_FragColor=vec4(noise2(uv*10.));return;
+	//gl_FragColor=vec4(printText(uv*80.));return;
 
 	vec3 c=vec3(0.);
 	float seed = t;
@@ -63,7 +137,7 @@ void main() {
 				lf = 3.,// * sin(t*.1), // focus distance
 				lS = 1.; // fov-ish
 	//vec3 sz = vec3(3.5,2.,3.);
-	float or = 4.;
+	float or = 8.;
 	mat3 mv = mat3(1,0,0,0,1,0,0,0,1);
 	//mv = RY(0.);
 	//mv = RY(2.*sin(t*.01))*RY(3.*sin(t*.039));
@@ -84,11 +158,12 @@ void main() {
 		vec3 at = vec3(uv*lS, O.z-lf);
 		D = normalize(at - O) * mv; O *= mv;
 		//O.z = 1.;
+		O += vec3(5., 0., 0.);
 		vec3 kc = vec3(1.);
 		float ins = 1.;
 		float hue = hash1(seed += P.x);
 		kc = hsv2rgb(vec3(hue,1.,1.));
-		float mskymat = mod(floor(tt/8.),2.);
+		float mskymat = mod(floor(tt/8.),4.);
 		float mballmat = mod(floor(tt/4.),4.);
 		float mfloormat = mod(floor(tt/6.),3.);
 		for (int i = 0; i < 6; ++i) {
@@ -106,7 +181,19 @@ void main() {
 				M = 1;
 			}
 
-			xsph(vec3(0.,-0.,1.), 4., 2, ins);
+			xsph(vec3(0.,0.,0.), 4., 2, ins);
+
+			float lp = -O.z / D.z;
+			if (lp > 0. && lp < l) {
+				vec3 p = O + D * lp;
+				if (mask(p.xy*8.)) {
+					P = p;
+					l = lp;
+					N = E.xxz;
+					UV = P.xy;
+					M = 4;
+				}
+			}
 
 			if (M == 0) {
 				if (mskymat == 0.) {
@@ -122,7 +209,12 @@ void main() {
 					me += .1 * vec3(.4, .1, .3) * pow(max(0., dot(D,bd)), 3.);
 					bd = normalize(vec3(1., .3, .3));
 					me += .1 * vec3(.1, .5, .4) * pow(max(0., dot(D,bd)), 4.);
+				} else {
+					me = vec3(0.);
 				}
+			} else if (M == 4) {
+				ma = vec3(0.);
+				me = vec3(10.);
 			} else if (M == 2) {
 				if (mballmat == 0.) {
 					ma = vec3(.8);
